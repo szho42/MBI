910b5168fd9f03b9449f29a03e0b035918ad71c5
Michael Eager <mick.eager@gmail.com>
2013-11-12 13:59:30 +1100

    add multiplication args to test.py

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import sys
 
 def main():
-  num = sys.argv[1:]
-  print 'hello git with num(' + str(num) + )  
+  cmd = sys.argv[1]
+  if cmd == 'add':
+     nums = sys.argv[2:]
+     total = sum(map(float,nums))
+  if cmd == 'mult':
+     nums = sys.argv[2:]
+     total = reduce(lambda a, b: a * b, map(float,nums))
+  print total 
 
 if __name__ == '__main__':
   main()
